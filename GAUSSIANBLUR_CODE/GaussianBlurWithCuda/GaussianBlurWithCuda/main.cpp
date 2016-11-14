0baa044f1d0f3287735a7a39cc1300dd10fb4782
erwenzhang <zhangvivien@126.com>
2016-11-14 14:53:21 -0600

    update image.h with Max's file

--- GAUSSIANBLUR_CODE/GaussianBlurWithCuda/GaussianBlurWithCuda/main.cpp
+++ GAUSSIANBLUR_CODE/GaussianBlurWithCuda/GaussianBlurWithCuda/main.cpp
@@ -41,14 +41,16 @@ __global__ void gaussianBlur(const unsigned char* inputChannel, unsigned char* c
 __global__ void separateChannels(const Image::Rgb* inputImageDevice, int numRows, int numCols, unsigned char* const redChannel,unsigned char* const greenChannel, unsigned char* const blueChannel){
     const int2 thread2DPos = make_int2(blockIdx.x*blockDim.x+threadIdx.x,blockIdx.y*blockDim.y+threadIdx.y);
     const int thread1DPos = thread2DPos.y * numCols + thread2DPos.x;
-    printf("hello from Thread: %d, %d, %d, %d\n", blockIdx.x, blockIdx.y, threadIdx.x, threadIdx.y);                                              
-    if(thread2DPos.x >= numCols ||thread2DPos.y >= numRows) return;
+    printf("hello from Thread: %d, %d, %d, %d\n", blockIdx.x, blockIdx.y, threadIdx.x, threadIdx.y);
+    printf("hello from Thread2: %d, %d, %d\n", thread2DPos.x, thread2DPos.y, thread1DPos);  
+      __syncthreads(); 
+    if(thread2DPos.x >= numCols ||thread2DPos.y >= numRows)     return;
     redChannel[thread1DPos] = inputImageDevice[thread1DPos].r;
     greenChannel[thread1DPos] = inputImageDevice[thread1DPos].g;
     blueChannel[thread1DPos] = inputImageDevice[thread1DPos].b;
 }
                                                               
-__global__ void combineChannels(const unsigned char* const redChannel,const unsigned char* const greenChannel, const unsigned char* const blueChannel, Image::Rgb* const outputImageDevice, int numRows,int numCols){
+__global__ void combineChannels(const unsigned char* const redChannel,const unsigned char* const greenChannel, const unsigned char* const blueChannel, Image::Rgb* const outputImageDevice, int numRows,int numCols,Image::Rgb tmpPixel){
     const int2 thread2DPos = make_int2(blockIdx.x*blockDim.x+threadIdx.x,blockIdx.y*blockDim.y+threadIdx.y);
     const int thread1DPos = thread2DPos.y * numCols + thread2DPos.x;
                                                               
@@ -57,116 +59,20 @@ __global__ void combineChannels(const unsigned char* const redChannel,const unsi
     unsigned char red = redChannel[thread1DPos];
     unsigned char green = greenChannel[thread1DPos];
     unsigned char blue = blueChannel[thread1DPos];
-    Image::Rgb pixel;
-    pixel.r = red; 
-    pixel.g = green;
-    pixel.b = blue;
-    outputImageDevice[thread1DPos] = pixel;
+    // Image::Rgb pixel;
+    tmpPixel.r = red; 
+    tmpPixel.g = green;
+    tmpPixel.b = blue;
+    outputImageDevice[thread1DPos] = tmpPixel;
 }
 
 Image imageInput;
 
-unsigned char *red,*green,*blue;
+// unsigned char *red,*green,*blue;
 float *filterDevice;
 Image::Rgb* inputImageDMarker;
 Image::Rgb* outputImageDMarker;
 float* filterHostMarker;
-
-void preProcess(const char *filename, Image::Rgb** inputImageHost){
-    
-//    checkCudaErrors(cudaFree(0));
-    printf("entering Preprocessing\n"); 
-    imageInput = readPPM("lena.ppm");
-    *inputImageHost = (Image::Rgb *)imageInput.pixels;
-}
-void postProcess(const char* output_file,Image::Rgb** outputImageHost ) { 
-    savePPM(outputImageHost, output_file);
-}
-
-void createFilterAndCopyToGPU(int* filterWidth, float** filterHost){
-    const int blurKernelWidth = 9;
-    const float blurKernelSigma = 2.;
-    *filterWidth = blurKernelWidth;
-    *filterHost = new float[blurKernelWidth*blurKernelWidth];
-    filterHostMarker = *filterHost;
-    
-    float filterSum = 0.f;
-    
-    for (int r = -blurKernelWidth/2; r <= blurKernelWidth/2; ++r) {
-        for (int c = -blurKernelWidth/2; c <= blurKernelWidth/2; ++c) {
-            float filterValue = expf( -(float)(c * c + r * r) / (2.f * blurKernelSigma * blurKernelSigma));
-            (*filterHost)[(r + blurKernelWidth/2) * blurKernelWidth + c + blurKernelWidth/2] = filterValue;
-            filterSum += filterValue;
-        }
-    }
-    
-    float normalizationFactor = 1.f / filterSum;
-    
-    for (int r = -blurKernelWidth/2; r <= blurKernelWidth/2; ++r) {
-        for (int c = -blurKernelWidth/2; c <= blurKernelWidth/2; ++c) {
-            (*filterHost)[(r + blurKernelWidth/2) * blurKernelWidth + c + blurKernelWidth/2] *= normalizationFactor;
-        }
-    }
-
-    checkCudaErrors(cudaMalloc((void**)&filterDevice,sizeof(float)*filterWidth*filterWidth));
-    checkCudaErrors(cudaMemcpy(filterDevice,filterHost,sizeof(float)*filterWidth*filterWidth,cudaMemcpyHostToDevice));
-    
-}
-
-void allocateMemoryAndCopyToGPU(Image::Rgb* inputImageDevice, Image::Rgb* outputImageDevice, unsigned char * redBlurredDevice,unsigned char * greenBlurredDevice, unsigned char* blueBlurredDevice){
-    const size_t numPixels = imageInput.rows*imageInput.cols;
-    checkCudaErrors(cudaMalloc((void**)&red,sizeof(unsigned char)*numPixels));
-    checkCudaErrors(cudaMalloc((void**)&green,sizeof(unsigned char)*numPixels));
-    checkCudaErrors(cudaMalloc((void**)&blue,sizeof(unsigned char)*numPixels));
-
-    checkCudaErrors(cudaMalloc((void**)&redBlurredDevice,    sizeof(unsigned char) * numPixels));
-    checkCudaErrors(cudaMalloc((void**)&greenBlurredDevice,  sizeof(unsigned char) * numPixels));
-    checkCudaErrors(cudaMalloc((void**)&blueBlurredDevice,   sizeof(unsigned char) * numPixels));
-    checkCudaErrors(cudaMemset(redBlurredDevice,   0, sizeof(unsigned char) * numPixels));
-    checkCudaErrors(cudaMemset(greenBlurredDevice, 0, sizeof(unsigned char) * numPixels));
-    checkCudaErrors(cudaMemset(blueBlurredDevice,  0, sizeof(unsigned char) * numPixels));
-
-    checkCudaErrors(cudaMalloc((void**)&inputImageDevice,sizeof(Image::Rgb)*numPixels));
-    checkCudaErrors(cudaMalloc((void**)&outputImageDevice,sizeof(Image::Rgb)*numPixels));
-    checkCudaErrors(cudaMemset(outputImageDevice,0,sizeof(Image::Rgb)*numPixels));
-    
-    checkCudaErrors(cudaMemcpy(inputImageDevice, inputImageHost,sizeof(Image::Rgb)*numPixels, cudaMemcpyHostToDevice));
-    
-    inputImageDMarker = inputImageDevice;
-    outputImageDMarker = outputImageDevice;  
-}
-
-void gaussianBlur(Image::Rgb* const inputImageDevice,
-                  Image::Rgb* const outputImageDevice,
-                  const size_t numRows,
-                  const size_t numCols,
-                  unsigned char * redBlurredDevice,
-                  unsigned char * greenBlurredDevice,
-                  unsigned char * blueBlurredDevice,
-                  const int filterWidth){
-    const dim3 blockSize(32,32,1);
-    const dim3 gridSize(numCols/32+1,numRows/32+1,1);
-    printf("entering the gaussianBlur\n");
-    printf("numCols is %d, numRows is %d, grid: %d, %d\n", numCols, numRows, numCols/32, numRows/32);
-    separateChannels<<<gridSize,blockSize>>>(inputImageDevice,numRows,numCols,red,green,blue);
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
-    
-    gaussianBlur<<<gridSize, blockSize>>>(red, redBlurredDevice, numRows, numCols, filterDevice, filterWidth);
-    gaussianBlur<<<gridSize, blockSize>>>(green, greenBlurredDevice, numRows, numCols, filterDevice, filterWidth);
-    gaussianBlur<<<gridSize, blockSize>>>(blue, blueBlurredDevice, numRows, numCols, filterDevice, filterWidth);
-
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
-
-    combineChannels<<<gridSize, blockSize>>>(redBlurredDevice,
-                                             greenBlurredDevice,
-                                             blueBlurredDevice,
-                                             outputImageDevice,
-                                             numRows,
-                                             numCols);
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
-}
-
-
 
 Image readPPM(const char *filename)
 {
@@ -182,8 +88,12 @@ Image readPPM(const char *filename)
         if (strcmp(header.c_str(), "P6") != 0) throw("Can't read input file");
         ifs >> w >> h >> b;
         img.w = w; img.h = h;
-	    imageInput.rows = h;
-	    imageInput.cols = w;
+        img.rows = h;
+        img.cols = w;
+
+        // printf("img.w = %d\n",img.cols);
+        // printf("img.h = %d\n",img.rows);
+
         img.pixels = new Image::Rgb[w * h]; // this is throw an exception if bad_alloc
         ifs.ignore(256, '\n'); // skip empty lines in necessary until we get to the binary data
         unsigned char pix[3];
@@ -214,10 +124,10 @@ void savePPM(Image::Rgb** outputImageHost, const char *filename)
         ofs << "P6\n" << imageInput.w << " " << imageInput.h << "\n255\n";
         unsigned char r, g, b;
         // loop over each pixel in the image, clamp and convert to byte format
-        for (int i = 0; i < img.w * img.h; ++i) {
-            r = static_cast<unsigned char>(std::min(1.f, outputImageHost[i].r) * 255);
-            g = static_cast<unsigned char>(std::min(1.f, outputImageHost[i].g) * 255);
-            b = static_cast<unsigned char>(std::min(1.f, outputImageHost[i].b) * 255);
+        for (int i = 0; i < imageInput.w * imageInput.h; ++i) {
+            r = static_cast<unsigned char>(std::min(1.f, outputImageHost[i]->r) * 255);
+            g = static_cast<unsigned char>(std::min(1.f, outputImageHost[i]->g) * 255);
+            b = static_cast<unsigned char>(std::min(1.f, outputImageHost[i]->b) * 255);
             ofs << r << g << b;
         }
         ofs.close();
@@ -228,27 +138,123 @@ void savePPM(Image::Rgb** outputImageHost, const char *filename)
     }
 }
 
-void cleanUp(void){
-    cudaFree(inputImageDMarker);
-    cudaFree(outputImageDMarker);
-    checkCudaErrors(cudaFree(filterDevice));
-
-    checkCudaErrors(cudaFree(red));
-    checkCudaErrors(cudaFree(green));
-    checkCudaErrors(cudaFree(blue));
+void preProcess(const char *filename, Image::Rgb** inputImageHost){
     
-
-    checkCudaErrors(cudaFree(redBlurredDecive));
-    checkCudaErrors(cudaFree(greenBlurredDevice));
-    checkCudaErrors(cudaFree(blueBlurredDevice));
-    delete[] filterHostMarker;
+//    checkCudaErrors(cudaFree(0));
+    printf("entering Preprocessing\n"); 
+    imageInput = readPPM("lena.ppm");
+    printf("image w=%d\n",imageInput.cols);
+    printf("image h=%d\n",imageInput.rows);
+    *inputImageHost = (Image::Rgb *)imageInput.pixels;
+    printf("pixel1: %d pixel2: %d pixel3: %d pixel4: %d", inputImageHost[0].r,inputImageHost[1].g,inputImageHost[2].b,inputImageHost[3].r);
 }
+void postProcess(const char* output_file,Image::Rgb** outputImageHost ) { 
+    savePPM(outputImageHost, output_file);
+}
+
+void createFilterAndCopyToGPU(int* filterWidth, float** filterHost){
+    const int blurKernelWidth = 9;
+    const float blurKernelSigma = 2.;
+    *filterWidth = blurKernelWidth;
+    *filterHost = new float[blurKernelWidth*blurKernelWidth];
+    filterHostMarker = *filterHost;
+    
+    float filterSum = 0.f;
+    
+    for (int r = -blurKernelWidth/2; r <= blurKernelWidth/2; ++r) {
+        for (int c = -blurKernelWidth/2; c <= blurKernelWidth/2; ++c) {
+            float filterValue = expf( -(float)(c * c + r * r) / (2.f * blurKernelSigma * blurKernelSigma));
+            (*filterHost)[(r + blurKernelWidth/2) * blurKernelWidth + c + blurKernelWidth/2] = filterValue;
+            filterSum += filterValue;
+        }
+    }
+    
+    float normalizationFactor = 1.f / filterSum;
+    
+    for (int r = -blurKernelWidth/2; r <= blurKernelWidth/2; ++r) {
+        for (int c = -blurKernelWidth/2; c <= blurKernelWidth/2; ++c) {
+            (*filterHost)[(r + blurKernelWidth/2) * blurKernelWidth + c + blurKernelWidth/2] *= normalizationFactor;
+        }
+    }
+
+    checkCudaErrors(cudaMalloc((void**)&filterDevice,sizeof(float)*(*filterWidth)*(*filterWidth)));
+    checkCudaErrors(cudaMemcpy(filterDevice,filterHost,sizeof(float)*(*filterWidth)*(*filterWidth),cudaMemcpyHostToDevice));
+    
+}
+
+void allocateMemoryAndCopyToGPU(Image::Rgb* inputImageHost, 
+                                Image::Rgb* inputImageDevice, 
+                                Image::Rgb* outputImageDevice, 
+                                unsigned char * redBlurredDevice,
+                                unsigned char * greenBlurredDevice, 
+                                unsigned char* blueBlurredDevice,
+                                unsigned char* red,
+                                unsigned char* green,
+                                unsigned char* blue,
+    ){
+    const size_t numPixels = imageInput.rows*imageInput.cols;
+    printf("numPixels =%d\n",numPixels );
+    checkCudaErrors(cudaMalloc((void**)&red,sizeof(unsigned char)*numPixels));
+    checkCudaErrors(cudaMalloc((void**)&green,sizeof(unsigned char)*numPixels));
+    checkCudaErrors(cudaMalloc((void**)&blue,sizeof(unsigned char)*numPixels));
+
+    // checkCudaErrors(cudaMalloc((void**)&redBlurredDevice,    sizeof(unsigned char) * numPixels));
+    // checkCudaErrors(cudaMalloc((void**)&greenBlurredDevice,  sizeof(unsigned char) * numPixels));
+    // checkCudaErrors(cudaMalloc((void**)&blueBlurredDevice,   sizeof(unsigned char) * numPixels));
+    // checkCudaErrors(cudaMemset(redBlurredDevice,   0, sizeof(unsigned char) * numPixels));
+    // checkCudaErrors(cudaMemset(greenBlurredDevice, 0, sizeof(unsigned char) * numPixels));
+    // checkCudaErrors(cudaMemset(blueBlurredDevice,  0, sizeof(unsigned char) * numPixels));
+
+    checkCudaErrors(cudaMalloc((void**)&inputImageDevice,sizeof(Image::Rgb)*numPixels));
+    // checkCudaErrors(cudaMalloc((void**)&outputImageDevice,sizeof(Image::Rgb)*numPixels));
+    // checkCudaErrors(cudaMemset(outputImageDevice,0,sizeof(Image::Rgb)*numPixels));
+    
+    checkCudaErrors(cudaMemcpy(inputImageDevice, inputImageHost,sizeof(Image::Rgb)*numPixels, cudaMemcpyHostToDevice));
+    
+    inputImageDMarker = inputImageDevice;
+    // outputImageDMarker = outputImageDevice;  
+}
+
+void gaussianBlur(Image::Rgb* const inputImageDevice,
+                  Image::Rgb* const outputImageDevice,
+                  const size_t numRows,
+                  const size_t numCols,
+                  unsigned char * redBlurredDevice,
+                  unsigned char * greenBlurredDevice,
+                  unsigned char * blueBlurredDevice,
+                  const int filterWidth){
+    const dim3 blockSize(32,32,1);
+    const dim3 gridSize(numCols/32,numRows/32,1);
+    printf("entering the gaussianBlur\n");
+    printf("numCols is %d, numRows is %d, grid: %d, %d\n", numCols, numRows, numCols/32, numRows/32);
+    // separateChannels<<<gridSize,blockSize>>>(inputImageDevice,numRows,numCols,red,green,blue);
+    // cudaDeviceSynchronize(); 
+    // checkCudaErrors(cudaGetLastError());
+ 
+    
+    // gaussianBlur<<<gridSize, blockSize>>>(red, redBlurredDevice, numRows, numCols, filterDevice, filterWidth);
+    // gaussianBlur<<<gridSize, blockSize>>>(green, greenBlurredDevice, numRows, numCols, filterDevice, filterWidth);
+    // gaussianBlur<<<gridSize, blockSize>>>(blue, blueBlurredDevice, numRows, numCols, filterDevice, filterWidth);
+
+    // cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    // Image::Rgb tmpPixel;
+    // combineChannels<<<gridSize, blockSize>>>(redBlurredDevice,
+    //                                          greenBlurredDevice,
+    //                                          blueBlurredDevice,
+    //                                          outputImageDevice,
+    //                                          numRows,
+    //                                          numCols,tmpPixel);
+    // cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+}
+
+
                   
 int main(int argc, const char * argv[]) {
     // insert code here...
     Image::Rgb* inputImageHost,*inputImageDevice;
     Image::Rgb* outputImageHost,*outputImageDevice;
     unsigned char* redBlurredDecive, *greenBlurredDevice, *blueBlurredDevice;
+    unsigned char* red, *green, *blue;
     
     float* filterHost;
     int filterWidth;
@@ -257,8 +263,13 @@ int main(int argc, const char * argv[]) {
     createFilterAndCopyToGPU(&filterWidth, &filterHost);
 	printf("after Preprocessing\n");   
   
-    allocateMemoryAndCopyToGPU(inputImageDevice,outputImageDevice,redBlurredDecive,greenBlurredDevice,blueBlurredDevice);
+    allocateMemoryAndCopyToGPU(inputImageHost, inputImageDevice,outputImageDevice,redBlurredDecive,greenBlurredDevice,blueBlurredDevice);
     
+
+    separateChannels<<<gridSize,blockSize>>>(inputImageDevice,imageInput.rows,imageInput.cols,red,green,blue);
+    cudaDeviceSynchronize(); 
+    checkCudaErrors(cudaGetLastError());
+
     GpuTimer timer;
     timer.Start();
     gaussianBlur(inputImageDevice, outputImageDevice, imageInput.rows, imageInput.cols,
@@ -277,7 +288,19 @@ int main(int argc, const char * argv[]) {
     checkCudaErrors(cudaMemcpy(outputImageHost, outputImageDevice, sizeof(Image::Rgb) * imageInput.rows*imageInput.cols, cudaMemcpyDeviceToHost));
     postProcess("result.ppm", &outputImageHost);
     
-    cleanUp();
+    cudaFree(inputImageDMarker);
+    cudaFree(outputImageDMarker);
+    checkCudaErrors(cudaFree(filterDevice));
+
+    checkCudaErrors(cudaFree(red));
+    checkCudaErrors(cudaFree(green));
+    checkCudaErrors(cudaFree(blue));
+    
+
+    checkCudaErrors(cudaFree(redBlurredDecive));
+    checkCudaErrors(cudaFree(greenBlurredDevice));
+    checkCudaErrors(cudaFree(blueBlurredDevice));
+    delete[] filterHostMarker;
     
     std::cout << "Hello, World!\n";
     return 0;
